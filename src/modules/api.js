37b511cc67ab5d5c19ca5fd4b81b7a2cc36b60d2
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-02-10 15:58:13 +0300

    add api fetch-post functions

---
+++ src/modules/api.js
@@ -0,0 +1,1 @@
+export const URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/B3Wj7c2iAcsIFlIQq8bR/scores/';
---
+++ src/modules/showScore.js
@@ -0,0 +1,48 @@
+import { URL } from './api.js';
+import { postScore } from './addScore.js';
+
+// Fetch Score
+export const fetchScore = async () => {
+  const response = await fetch(
+    URL,
+  );
+  const getScore = await response.json();
+  return getScore.result;
+};
+
+const listContainer = document.querySelector('.names-score-list-container');
+
+// Show Scores
+export const showScore = async () => {
+  const data = await fetchScore();
+  data.sort((a, b) => b.score - a.score);
+  listContainer.innerHTML = '';
+  const tableData = data.map(item => `<tr> <td id="table-user">${item.user}</td> <td id="table-score">${item.score}</td> </tr>`
+  );
+  listContainer.innerHTML = `<table><tbody>${tableData.join('')}</tbody></table>`;
+  };
+
+// Form Submit
+const nameInput = document.querySelector('#name');
+const scoreInput = document.querySelector('#score');
+const formSubmitBtn = document.querySelector('#form-submit-btn');
+
+export const formSubmit = () => {
+formSubmitBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  postScore();
+  nameInput.value = '';
+  scoreInput.value = '';
+});
+};
+
+// Refresh List
+const refreshButton = document.getElementById('refresh-btn');
+
+export const refreshList = () => {
+refreshButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  fetchScore();
+  showScore();
+});
+};
